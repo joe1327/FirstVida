107ee735e02fea50534bb0c190aee09d4ac9027e
mariofnl <toktoktoken@gmail.com>
2015-02-08 18:31:57 +0100

    [FEATURE] Fragments

--- app/src/main/java/firstvida/com/firstvida/Main.java
+++ app/src/main/java/firstvida/com/firstvida/Main.java
@@ -45,7 +45,7 @@ public class Main extends ActionBarActivity
 
         Fragment aux = new Fragment();
 
-        switch (position){
+        switch (position+1){
             case 1:
                 aux = ProfileFragment1.newInstance(position);
                 break;
@@ -81,10 +81,10 @@ public class Main extends ActionBarActivity
                 mTitle = getString(R.string.title_section3);
                 break;
             case 4:
-                mTitle = getString(R.string.title_section3);
+                mTitle = getString(R.string.title_section4);
                 break;
             case 5:
-                mTitle = getString(R.string.title_section3);
+                mTitle = getString(R.string.title_section5);
                 break;
         }
     }
